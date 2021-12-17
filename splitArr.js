001d1d0fdadc59851edb220e22ebb26d738c1403
THEiDiet <76583093+THEiDiet@users.noreply.github.com>
2021-12-17 22:39:33 +0700

    done some lessons

---
+++ splitArr.js
@@ -0,0 +1,16 @@
+function chunkArrayInGroups(arr, size) {
+    const sliced_array = []
+    for (let i = 0; i <arr.length; i += size) {
+        sliced_array.push(arr.slice(i, i + size));
+    }
+    return sliced_array
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+
+
+
+
+
+
+console.log(sliced_array);
